94c55d143df93725a7e0219b5fe918a690c31081
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-08-15 17:39:58 -0300

    Use extract_value

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from layout import *
 from image_to_graph import *
+from extract_values import *
 
 
 def main():
     image = open_image('new.jpg')
     g = image_to_graph(image)
     mst = g.spanning_tree(weights=g.es['weight'], return_tree=True)
-    show_graph_and_mst(g, mst)
+    values = extract_values(mst.es['weight'])
 
 
 def open_image(filename):
-    return io.imread(filename)
+    return io.imread(filename, as_gray=True)
 
 
 if __name__:
