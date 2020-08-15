9d0a2eeeef7337c597120772a195ff25e2881439
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-08-15 17:39:35 -0300

    Add extract_values.py

---
+++ extract_values.py
@@ -0,0 +1,54 @@
+from statistics import stdev, mean
+from scipy.stats import skew, kurtosis, entropy, energy_distance
+
+
+# Cálculo vetores de características
+def extract_values(values, is_dictionary=True):
+    if is_dictionary:
+        return {
+            'averate': calc_average(values),
+            'standard_deviation': calc_standard_deviation(values),
+            'skew': calc_skew(values),
+            'kurtosis': calc_kurtosis(values),
+            'energy': calc_energy(values),
+            'entropy': calc_entropy(values)
+        }
+    else:
+        return [calc_average(values),
+                calc_standard_deviation(values),
+                calc_skew(values),
+                calc_kurtosis(values),
+                calc_energy(values),
+                calc_entropy(values)
+                ]
+
+
+# Cálculo da média
+def calc_average(values):
+    return mean(values)
+
+
+# Cálculo do desvio padrão
+def calc_standard_deviation(values):
+    return stdev(values)
+
+
+# Cálculo do grau de assimetria
+def calc_skew(values):
+    return skew(values)
+
+
+# Cálculo da curtose
+def calc_kurtosis(values):
+    return kurtosis(values)
+
+
+# Cálculo da entropia
+def calc_entropy(values):
+    return entropy(values)
+
+
+# Cálculo da energia
+def calc_energy(values):
+    return ''
+    # return energy_distance(values, )
