7eaca805468c4c48f735d2584984bda4298f1a83
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2020-08-23 23:52:24 -0300

    Add image class at CSV

--- images_to_csv.py
+++ images_to_csv.py
@@ -5,15 +5,24 @@ from extract_values import extract_values
 from csv import DictWriter
 
 
-def image_to_csv(dir='/mnt/5022A63622A620C8/TCC/tests/datasets/KylbergTextureDataset/*.png'):
+def image_to_csv(dir='/mnt/5022A63622A620C8/TCC/tests/datasets/kylberg-texture-dataset/images/*.png'):
     images = glob(dir)
     dic = []
     print(f"Iniciando extração de {len(images)} imagens...")
     for i, image in enumerate(images):
         print(f"- Imagem {i + 1}")
+
+        file_name = get_file_name(image)
+        image_class = get_image_class(file_name)
+
         image = open_image(image)
         mst, g = image_to_mst(image)
-        dic.append(extract_values(mst.es['weight']))
+
+        values = extract_values(mst.es['weight'])
+        values['class'] = image_class
+
+        dic.append(values)
+
     print('Salvando arquivo CSV...')
     save_csv(dic)
     print('Finalizado: arquivo salvo em: /mnt/5022A63622A620C8/TCC/tests/mst.csv')
@@ -29,6 +38,14 @@ def save_csv(dic, csv_file='/mnt/5022A63622A620C8/TCC/tests/mst.csv'):
                 writer.writerow(row)
     except IOError:
         print("I/O error")
+
+
+def get_file_name(path):
+    return path.split('/')[-1]
+
+
+def get_image_class(image_file_name):
+    return image_file_name.split('_')[0]
 
 
 if __name__:
